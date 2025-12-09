a2c8448ea6cda8231c63a1eb7be3a25270bfdb5c
ankur <ankursawant811@gmail.com>
2025-12-09 13:19:27 +0530

    added blog page

---
+++ frontend/src/pages/blog.jsx
@@ -0,0 +1,12 @@
+export default function Blog() {
+  return (
+    <div className="container">
+      <form>
+        <input />
+        <input />
+        <textarea />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
