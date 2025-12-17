b3c5219231cd47b365073d2ef806d50bd9750f5e
ankur <ankursawant811@gmail.com>
2025-12-17 14:52:49 +0530

    remove unnecessary code

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,6 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.static("dist"));
-app.use();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/api/", blogRouter);
